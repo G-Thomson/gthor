9fb97e17fa1ea70c492d057c0a79f8cf100d844c
Geoffrey Thomson <gtho123@aucklanduni.ac.nz>
2017-03-12 19:46:10 +1300

    change back to dat to subset list

--- R/flowering_time.R
+++ R/flowering_time.R
@@ -156,18 +156,19 @@ flr.hist.graph <- function(data, meas = "fl", new.titles = NULL){
   require(scales)
   require(ggthemes)
 
-  data <- data$Raw
+  # Subset list
+  dat <- data$Raw
 
   # Creates the basic plots based on measurment of flowering
   if(meas == "nd"){
-    plot <- ggplot(data=data, aes(Node_num, fill = Mutant)) +
-      geom_histogram(data=subset(data, Genotype!="R108"),aes(y=..count..), binwidth = 1) +
-      geom_histogram(data=subset(data, Genotype=="R108"),aes(x = Node_num, y=..count..), binwidth = 1)
+    plot <- ggplot(data=dat, aes(Node_num, fill = Mutant)) +
+      geom_histogram(data=subset(dat, Genotype!="R108"),aes(y=..count..), binwidth = 1) +
+      geom_histogram(data=subset(dat, Genotype=="R108"),aes(x = Node_num, y=..count..), binwidth = 1)
   }
   else if(meas == "fl"){
-    plot <- ggplot(data=data, aes(Days_to_flower, fill = Mutant)) +
-      geom_histogram(data=subset(data, Genotype!="R108"),aes(y=..count.., fill = colr), binwidth = 1) +
-      geom_histogram(data=subset(data, Genotype=="R108"),aes(x = Days_to_flower, y=..count.., fill = colr), binwidth = 1)
+    plot <- ggplot(data=dat, aes(Days_to_flower, fill = Mutant)) +
+      geom_histogram(data=subset(dat, Genotype!="R108"),aes(y=..count.., fill = colr), binwidth = 1) +
+      geom_histogram(data=subset(dat, Genotype=="R108"),aes(x = Days_to_flower, y=..count.., fill = colr), binwidth = 1)
   }
   else{ stop("ERROR: Please specify a correct measurment of flowering. See docs for details.") }
 
