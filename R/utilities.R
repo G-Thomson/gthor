bd1462d9e088d7edd898353797f8b60c9eba9905
Geoffrey Thomson <gtho123@aucklanduni.ac.nz>
2017-10-16 09:53:55 +1300

    add list_to_df

--- R/utilities.R
+++ R/utilities.R
@@ -75,3 +75,20 @@ moe <- function(data, alpha = 0.05){
 px_to_mm <- function(n){
   n*0.264583
 }
+
+#' list_to_df
+#'
+#' @export
+list_to_df <- function(listfordf){
+  if(!is.list(listfordf)) stop("it should be a list")
+
+  df <- list(list.element = listfordf)
+  class(df) <- c("tbl_df", "data.frame")
+  attr(df, "row.names") <- .set_row_names(length(listfordf))
+
+  if (!is.null(names(listfordf))) {
+    df$name <- names(listfordf)
+  }
+
+  df %>% unnest(list.element)
+}
